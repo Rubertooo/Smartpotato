3264895c5fae6b5e55cef6c527a8434b6b5de9ff
Ruben Thys <27220@student.tisp.be>
2025-01-30 21:10:12 +0100

    dlfkjqpsoihe

--- scripts/sprite.gd
+++ scripts/sprite.gd
@@ -1,17 +1,19 @@
 extends CharacterBody2D
+@onready var area_2d: Area2D = $"../StaticBody2D/Area2D"
+@onready var animate: AnimatedSprite2D = $AnimatedSprite2D
 
 
 const SPEED = 5.0
 const JUMP_VELOCITY = -400.0
 var first_position
 var last_position
+var animation
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
 	# Handle jump.
 	if Input.is_action_just_pressed("Left click") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -32,3 +34,12 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	get_tree().reload_current_scene()
+
+
+func _on_knife_area_body_entered(body: Node2D) -> void:
+	print("aaaaa")
+	animate.play("Peeled1")
+	
